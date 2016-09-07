fdd9d46f10dfdfbdc7de03bc4beac7cadcd3f05e
songqiaoyi <ss1299@163.com>
2016-09-08 00:05:42 +0800

    task7 complete

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,58 @@
-/// 包装函数
 module.exports = function(grunt) {
-  // 任务配置,所有插件的配置信息
+  // LiveReload的默认端口号，你也可以改成你想要的端口号
+  var lrPort = 35729;
+  // 使用connect-livereload模块，生成一个与LiveReload脚本
+  // <script src="http://127.0.0.1:35729/livereload.js?snipver=1" type="text/javascript"></script>
+  var lrSnippet = require('connect-livereload')({
+    port: lrPort
+  });
+  // 使用 middleware(中间件)，就必须关闭 LiveReload 的浏览器插件
+  var serveStatic = require('serve-static');
+  var serveIndex = require('serve-index');
+  var lrMiddleware = function(connect, options, middlwares) {
+    return [
+      lrSnippet,
+      // 静态文件服务器的路径
+      serveStatic(options.base[0]),
+      // 启用目录浏览(相当于IIS中的目录浏览)
+      serveIndex(options.base[0])
+    ];
+  };
+
+  // 项目配置(任务配置)
   grunt.initConfig({
+    // 读取我们的项目配置并存储到pkg属性中
     pkg: grunt.file.readJSON('package.json'),
+    // 通过connect任务，创建一个静态服务器
+    connect: {
+      options: {
+        // 服务器端口号
+        port: 8000,
+        // 服务器地址(可以使用主机名localhost，也能使用IP)
+        hostname: 'localhost',
+        // 物理路径(默认为. 即根目录) 注：使用'.'或'..'为路径的时，可能会返回403 Forbidden. 此时将该值改为相对路径 如：/grunt/reloard。
+        base: '.'
+      },
+      livereload: {
+        options: {
+          // 通过LiveReload脚本，让页面重新加载。
+          middleware: lrMiddleware
+        }
+      }
+    },
     stylus:{
       build: {
         options: {
           linenos: false,
-          compress: true
+          compress: false
         },
         files: [{
-          'src/css/task6.css': ['src/css/task6.styl']
+          'src/css/task6.css': ['src/css/task6.styl'],
+          'src/css/task7.css': ['src/css/task7.styl']
         }]
       }
     },
-    // watch插件的配置信息
+    // 通过watch任务，来监听文件是否有更改
     watch: {
       another: {
         files: ['css/*','src/css/*.styl'],
@@ -22,12 +60,24 @@ module.exports = function(grunt) {
         options: {
           livereload: 1337
         }
+      },
+      client: {
+        // 我们不需要配置额外的任务，watch任务已经内建LiveReload浏览器刷新的代码片段。
+        options: {
+          livereload: lrPort
+        },
+        // '**' 表示包含所有的子目录
+        // '*' 表示包含所有的文件
+        files: ['src/*.html', 'src/css/*.css', 'src/js/*.js']
       }
     }
-  });
-  // 告诉grunt我们将使用插件
+  }); // grunt.initConfig配置完毕
+
+  // 加载插件
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-stylus');
-  // 告诉grunt当我们在终端中输入grunt时需要做些什么
-  grunt.registerTask('default', ['watch']);
+
+  // 自定义任务
+  grunt.registerTask('default', ['connect', 'watch']);
 };
